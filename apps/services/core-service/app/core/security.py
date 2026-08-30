99a81483b9d2225f6b030ec4b3b4cdaef69a537b
ir296545-afk <ir296545@gmail.com>
2026-08-30 10:39:44 -0300

    feat: adiciona geração de token JWT

--- apps/services/core-service/app/core/security.py
+++ apps/services/core-service/app/core/security.py
@@ -15,3 +15,18 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 SECRET_KEY = "chave-temporaria"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict) -> str:
+  to_encode = data.copy()
+
+  expire = datetime.now(timezone.utc) + timedelta(
+    minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+  )
+
+  to_encode.update({"exp": expire})
+
+  return jwt.encode(
+    to_encode,
+    SECRET_KEY,
+    algorithm=ALGORITHM
+  )
