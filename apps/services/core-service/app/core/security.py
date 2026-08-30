a1e571db2d883e2c55fb12454fbe859f54f677f7
José Ivan Luz Ramos <ir296545@gmail.com>
2026-08-30 13:09:21 -0300

    feat: completa seguranca JWT com bcrypt

--- apps/services/core-service/app/core/security.py
+++ apps/services/core-service/app/core/security.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta, timezone
 
 import jwt
 from passlib.context import CryptContext
+from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -12,21 +13,25 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
   return pwd_context.verify(plain_password, hashed_password)
 
-SECRET_KEY = "chave-temporaria"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict) -> str:
   to_encode = data.copy()
 
   expire = datetime.now(timezone.utc) + timedelta(
-    minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
   )
 
   to_encode.update({"exp": expire})
 
   return jwt.encode(
     to_encode,
-    SECRET_KEY,
-    algorithm=ALGORITHM
+    settings.SECRET_KEY,
+    algorithm=settings.ALGORITHM
+  )
+
+def decode_access_token(token: str) -> dict:
+  return jwt.decode(
+    token,
+    settings.SECRET_KEY,
+    algorithms=[settings.ALGORITHM]
   )
